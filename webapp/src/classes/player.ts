1bf19620ac1df24c86b0a3e73924188e849cc3fc
kintrix007 <kintrix007@proton.me>
2024-05-25 21:22:26 +0200

    Move player sprite into a field of player

--- webapp/src/classes/player.ts
+++ webapp/src/classes/player.ts
@@ -1,5 +1,10 @@
 export class Player {
-    constructor() {
-        let level = 0;
-    }
+  sprite: HTMLImageElement
+  level: number
+
+  constructor() {
+    this.sprite = document.createElement("img");
+    this.sprite.src = "/assets/player.png";
+    this.level = 1;
+  }
 }
