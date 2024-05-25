2be442ba725f646673d3feeb92849de9a22123a6
kintrix007 <kintrix007@proton.me>
2024-05-25 20:39:07 +0200

    Added logic for update and events

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,12 +1,19 @@
 import './style.css';
-import './classes/gamemanager';
+import { initialize, onKeydown, update } from './classes/gamemanager'; 
 
+function main() {
+  initialize();
 
+  function loop(timestamp) {
+    update(timestamp);
+    window.requestAnimationFrame(loop);
+  }
 
+  window.requestAnimationFrame(loop);
 
+  document.addEventListener("keydown", ev => {
+    onKeydown(ev);
+  });
+}
 
-
-
-
-
-
+main();
